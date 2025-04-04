2240dc25d5652ed833c4561a71d440c754ed5e78
Aydan Thompson <aydanthompson20@gmail.com>
2025-04-04 23:11:54 +0100

    Added oil lantern refilling

--- Sailwind - Smart Lanterns/Patches/LanternRefill.cs
+++ Sailwind - Smart Lanterns/Patches/LanternRefill.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -43,33 +43,36 @@ namespace Smart_Lanterns.Patches
 
     internal static class LanternRefill
     {
-
-        static bool Prefix(ShipItemLight __instance, ref int ___health, ref int ___initialHealth)
+        static bool Prefix(ShipItemLight __instance, ref float ___health, ref float ___initialHealth)
         {
-            if (__instance.usesOil) return true;// we're not touching oil lanterns, just let the original method run unchanged.
-            //if (!Plugin.refillCandleLanterns.Value) return true; //per config, user requested to not refill the candle lanterns so do nothing, just let the original method run unchanged.
-            
             if (___health <= 0f)
             {
-                //FileLog.Log("health <= 0, triggering candle restock.");
-                bool result = consumeCandle();
-                if (result)
-                {
-                    ___health = ___initialHealth;//we succesfully removed a candle from storage, so refill the lantern.
-                    //FileLog.Log("Lantern restocked succesfully.");
+                float healthBeforeRefill = ___health;
 
-                    //try turning it on again to prevent the bug where it turns off when refilling.
+                if (__instance.usesOil)
+                {
+                    ConsumeOil(__instance, ref ___health, ref ___initialHealth);
+                }
+                else
+                {
+                    bool result = consumeCandle();
+                    if (result) { ___health = ___initialHealth; }  // We successfully removed a candle from storage, so refill the lantern.
+                }
+
+                if (___health != healthBeforeRefill)
+                {
+                    // If the lantern health has changed, try turning it on again
+                    // to prevent the bug where it turns off when refilling.
                     MethodInfo setLightMethod = typeof(ShipItemLight).GetMethod("SetLight", BindingFlags.NonPublic | BindingFlags.Instance);
                     if (setLightMethod != null)
                     {
-                        setLightMethod.Invoke(__instance, new object[] { true });                        
+                        setLightMethod.Invoke(__instance, new object[] { true });
                     }
                 }
             }
 
             //FileLog.Log("candle health is " + ___health);
             return true; // if a candle ran out, we already replaced it, so allow the original code to run. as far as it's concerned, the candle never ran out.
-            
         }
 
         private static bool consumeCandle()
@@ -92,5 +95,56 @@ namespace Smart_Lanterns.Patches
                 return false; // could not find a candle to use.
         }
 
+        private static bool ConsumeOil(ShipItemLight __instance, ref float ___health, ref float ___initialHealth)
+        {
+            // Find nearby oil bottles.
+            Vector3 lanternPosition = __instance.transform.position;
+            float searchRadius = 20f;  // Large enough to cover the entirety of the Brig.
+            List<ShipItemLanternFuel> fuels = FindNearbyOilBottles(lanternPosition, searchRadius);
+
+            // No oil bottles found, can't refill fuel.
+            if (fuels.Count == 0) { return false; }
+
+            float remainingFuelRequired = ___initialHealth - ___health;
+            foreach (ShipItemLanternFuel fuel in fuels)
+            {
+                if (fuel.health >= remainingFuelRequired)
+                {
+                    // Oil bottle has more than the required fuel, so refill
+                    // the lantern and stop looking for more fuel.
+                    fuel.health -= remainingFuelRequired;
+                    ___health = ___initialHealth;
+                    return true;
+                }
+                else
+                {
+                    // Oil bottle doesn't have enough fuel, so use up the fuel from
+                    // this bottle and continue to the next fuel.
+                    remainingFuelRequired -= fuel.health;
+                    ___health += fuel.health;
+                    fuel.health = 0f;
+                }
+            }
+
+            // Unable to completely fill the lantern.
+            return false;
+        }
+
+        private static List<ShipItemLanternFuel> FindNearbyOilBottles(Vector3 position, float radius)
+        {
+            // Find all oil bottles within a certain radius (in a sphere) of
+            // the provided position (likely the lantern).
+            List<ShipItemLanternFuel> nearbyFuels = [];
+            Collider[] colliders = Physics.OverlapSphere(position, radius);
+            foreach (Collider collider in colliders)
+            {
+                ShipItemLanternFuel fuel = collider.GetComponent<ShipItemLanternFuel>();
+                if (fuel != null && fuel.oilBottle)
+                {
+                    nearbyFuels.Add(fuel);
+                }
+            }
+            return nearbyFuels;
+        }
     }
 }
